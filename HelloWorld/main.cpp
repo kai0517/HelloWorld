fb9b507c1eebc2dbbd45f3bfdb5b8edcd151ed04
Chang Wen-Kai <kai0517@gmail.com>
2016-06-08 01:33:08 +0800

    Add curly-brace-delimited initializer and auto code samples

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
+void TestCurlyBraceDelimitedInitializerAndAuto()
+{
+	int i{ 10 };
+	// error C2397: conversion from 'double' to 'int' requires a narrowing conversion
+	// int i2{ 3.14 }; 
+	std::cout << "The value of 'i' is " << i << "\n";
+
+	double d{ 3.14159 };
+	std::cout << "The value of 'd' is " << d << "\n";
+
+	std::vector< int > v{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	std::cout << "The value of integer vector 'v' is [";
+	for (auto it = v.begin(); it != v.end(); ++it)
+	{
+		std::cout << (it == v.begin() ? "" : ",") << *it;
+	}
+	std::cout << "]\n";
+}
 
 int main(int argc, char* argv[])
 {
@@ -7,6 +27,8 @@ int main(int argc, char* argv[])
 	std::string name;
 	std::cin >> name;
 	std::cout << "Hello, " << name << "." << std::endl;
+
+	TestCurlyBraceDelimitedInitializerAndAuto();
 
 	system("pause");
 	return 0;
